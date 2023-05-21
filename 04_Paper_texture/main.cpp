a0630931bdf6b3fad056de63a18ca262111777e3
DiegoAGtz <diegogtz1207@gmail.com>
2023-05-20 21:14:15 -0600

    Más o menos funciona

--- 04_Paper_texture/main.cpp
+++ 04_Paper_texture/main.cpp
@@ -1,39 +1,54 @@
-#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <imgcodecs.hpp>
+#include <iostream>
+
 #include <core.hpp>
 #include <highgui.hpp>
-#include <imgcodecs.hpp>
 #include <imgproc.hpp>
-#include <iostream>
-#include <opencv.hpp>
-#include <string>
 
 using namespace cv;
 using namespace std;
 
-int get_sdh(const Mat &src, Mat &sh, Mat &dh, int d, int theta);
+int getSDH(const Mat &src, Mat &sh, Mat &dh, int d);
+int normHist(const Mat &src, Mat &dst);
+float getHomogeneity(const Mat &src);
+int getDisplacementVectors(const Mat &src, Mat &hdst, Mat &vdst);
+int writeCSV(string filename, Mat m);
+int getTexelSize(const Mat &dsvect);
 
-const string img_source = "./images/test.jpg";
-const Mat example =
-    (Mat_<uchar>(5, 5) << 0, 255, 0, 0, 0, 255, 255, 255, 0, 255, 255, 255, 0,
-     255, 255, 255, 0, 255, 255, 255, 0, 255, 255, 0, 255);
+const string img_source = "./images/IMG_0215.jpg";
 
 int main(void) {
   Mat input = imread(img_source, IMREAD_GRAYSCALE);
-  Mat sh, dh;
-  get_sdh(example, sh, dh, 2, 0);
-  cout << "Sums" << endl << sh << endl;
-  cout << "Sums" << endl << dh << endl;
-  cout << "Sums - total elements" << endl << sum(sh)[0] << endl;
-  cout << "Diffs - total elements" << endl << sum(dh)[0] << endl;
-  imshow("Sums", sh);
-  imshow("Differences", dh);
+  Mat inputColor = imread(img_source, IMREAD_COLOR);
+  Mat hdv, vdv;
+  getDisplacementVectors(input, hdv, vdv);
+
+  double htexels = getTexelSize(hdv);
+  double vtexels = getTexelSize(vdv);
+
+  cout << "Horizontal texel: " << htexels << endl;
+  cout << "Vertical texel: " << vtexels << endl;
+
+  rectangle(inputColor, Point(0, 0), Point(htexels, vtexels), Scalar(0, 0, 255),
+            2);
+  rectangle(inputColor, Point(htexels, 0), Point(htexels * 2, vtexels),
+            Scalar(0, 0, 255), 2);
+  rectangle(inputColor, Point(0, vtexels), Point(htexels, vtexels * 2),
+            Scalar(0, 0, 255), 2);
+  rectangle(inputColor, Point(htexels, vtexels),
+            Point(htexels * 2, vtexels * 2), Scalar(0, 0, 255), 2);
+
+  if (inputColor.rows > 1000 || inputColor.cols > 1000)
+    resize(inputColor, inputColor, Size(), 0.5, 0.5);
+  imshow("Texel", inputColor);
   waitKey(0);
   return 0;
 }
 
-int get_sdh(const Mat &src, Mat &sh, Mat &dh, int d, int theta) {
+int getSDH(const Mat &src, Mat &sh, Mat &dh, int d) {
   int histSize = 511; // number of bins in the histogram
-
   // calculate the histogram
   sh = Mat::zeros(1, histSize, CV_16UC1);
   dh = Mat::zeros(1, histSize, CV_16UC1);
@@ -41,10 +56,88 @@ int get_sdh(const Mat &src, Mat &sh, Mat &dh, int d, int theta) {
     uchar *p_src = (uchar *)src.ptr<uchar>(r);
     ushort *p_shist = (ushort *)sh.ptr<ushort>(0);
     ushort *p_dhist = (ushort *)dh.ptr<ushort>(0);
-    for (int c = 0; c < src.cols - 1; c++) {
-      p_shist[p_src[c] + p_src[c + 1]]++;
-      p_dhist[p_src[c] - p_src[c + 1] + 255]++;
+    for (int c = 0; c < src.cols - d; c++) {
+      p_shist[p_src[c] + p_src[c + d]]++;
+      p_dhist[p_src[c] - p_src[c + d] + 255]++;
     }
   }
   return 0;
+}
+
+int normHist(const Mat &src, Mat &dst) {
+  int A = sum(src)[0];
+  src.copyTo(dst);
+  dst.convertTo(dst, CV_32FC1);
+  dst /= A;
+  return 0;
+}
+
+float getHomogeneity(const Mat &src) {
+  float homogeneity = 0;
+  float *row = (float *)src.ptr<float>(0);
+  for (int c = 0; c < src.cols; c++) {
+    homogeneity += (1.0 / (1 + ((c - 255) * (c - 255))) * row[c]);
+  }
+  return homogeneity;
+}
+
+int getDisplacementVectors(const Mat &src, Mat &hdst, Mat &vdst) {
+  hdst = Mat(1, src.cols, CV_64FC1);
+  vdst = Mat(1, src.rows, CV_64FC1);
+
+  ofstream horHomogeneityFile("out/yo_horHomogeneity.csv", ios::out),
+      verHomogeneityFile("out/yo_verHomogeneity.csv", ios::out);
+
+  Mat sh, dh;
+  Mat hdata_x(1, src.cols, CV_64FC1);
+  double *dst = (double *)hdst.ptr<double>(0);
+  for (int d = 1; d < src.cols; d++) {
+    getSDH(src, sh, dh, d);
+    normHist(dh, dh);
+    dst[d] = getHomogeneity(dh);
+    horHomogeneityFile << dst[d] << ((d + 1) < src.cols ? "," : "");
+  }
+
+  Mat srcpy;
+  src.copyTo(srcpy);
+  transpose(srcpy, srcpy);
+  Mat vdata_x = Mat(1, srcpy.cols, CV_64FC1);
+  dst = (double *)vdst.ptr<double>(0);
+  for (int d = 1; d < srcpy.cols; d++) {
+    getSDH(srcpy, sh, dh, d);
+    normHist(dh, dh);
+    dst[d] = getHomogeneity(dh);
+    verHomogeneityFile << dst[d] << ((d + 1) < srcpy.cols ? "," : "");
+  }
+
+  horHomogeneityFile.close();
+  verHomogeneityFile.close();
+  return 0;
+}
+
+int writeCSV(string filename, Mat m) {
+  ofstream myfile;
+  myfile.open(filename.c_str());
+  myfile << cv::format(m, cv::Formatter::FMT_CSV) << std::endl;
+  myfile.close();
+  return 0;
+}
+
+int getTexelSize(const Mat &dsvect) {
+  double *p = (double *)dsvect.ptr<double>(0);
+  double max = p[10];
+  int ret = 10;
+  bool up = false;
+  for (int c = 10; c < dsvect.cols - 1; c++) {
+    if (p[c] > max) {
+      max = p[c];
+      ret = c;
+      up = true;
+    }
+    if (up && p[c] < max) {
+      cout << p[c] << " - " << max << " - " << c << endl;
+      break;
+    }
+  }
+  return ret;
 }
---
+++ 04_Paper_texture/plot.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+def read_file(filename):
+	out = []
+	with open(filename, "r") as file:
+		for row in file:
+			out.append(row.strip().split(","))
+	
+	return out
+
+img_name = sys.argv[1]
+
+horHomoCues = [float(num) for num in read_file(f"out/{img_name}_horHomogeneity.csv")[0]]
+verHomoCues = [float(num) for num in read_file(f"out/{img_name}_verHomogeneity.csv")[0]]
+x = np.arange(0, len(horHomoCues))
+x2 = np.arange(0, len(verHomoCues))
+
+plt.figure(f"{img_name} homogeneity", dpi = 120)
+plt.title(f"Homogeneity with variable displacement ({img_name})")
+plt.xlabel("Displacement/Offset")
+plt.ylabel("Homogeneity")
+plt.plot(x2, verHomoCues, ".-", color = "red", label = "Vertical")
+plt.plot(x, horHomoCues, ".-", color = "blue", label = "Horizontal")
+plt.grid(color = "darkgray", linestyle = "dashed", linewidth = "0.5")
+plt.legend()
+
+try:
+	arg = sys.argv[2]
+	match arg:
+		case "--save":
+			if(not os.path.isdir("./out/img/")):
+				os.mkdir("./out/img/")
+				plt.savefig(f"./out/img/{img_name}_homogeneity.png", dpi = 280)
+			else:
+				plt.savefig(f"./out/img/{img_name}_homogeneity.png", dpi = 280)
+
+		case "--show":
+			plt.show()
+
+		case "--save-and-show":
+			if(not os.path.isdir("./out/img/")):
+				os.mkdir("./out/img/")
+				plt.savefig(f"./out/img/{img_name}_homogeneity.png", dpi = 280)
+				plt.show()
+			else:
+				plt.savefig(f"./out/img/{img_name}_homogeneity.png", dpi = 280)
+				plt.show()
+
+		case _:
+			print(
+	    """Error while processing arguments. You can:
+\t--save: Save figure in out/img/.
+\t--show: Opens a window showing the figure.
+\t--save-and-show: Save figure in out/img/ and open a window showing the figure.
+			""")
+except:
+  pass
